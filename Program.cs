081e42d6450615d168601a5e88385fd610d81b83
tomasoyarzabal <tomasoyarzabal135@gmail.com>
2022-08-09 16:59:04 -0300

    calculadora

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1,n2,r;
+            Console.WriteLine("ingrese un numero");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese otro numero");
+            n2 = int.Parse(Console.ReadLine());
+            r = n1 + n2;
+            Console.WriteLine("El resultado es " + r );
+            
+
+
+        }
+    }
+}
